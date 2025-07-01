5675956b0434fa91e9cf7b69fe1dfc6886a815d5
Karlis Strautnieks <karlisstrautnieks2004@gmail.com>
2025-07-01 16:27:38 +0300

    JSX + lint update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-const Pet = (props) => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", null, props.name),
-    React.createElement("h3", null, props.animal),
-    React.createElement("h3", null, props.breed)
-  );
-};
+import ReactDOM from "react-dom/client";
+import Pet from "./Pet";
 const App = () => {
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h1", null, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-    React.createElement(Pet, { name: "Sparky", animal: "Dog", breed: "Mixed" })
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Cat" breed="mixd" />
+      <Pet name="Doink" animal="bird" breed="cockateil" />
+    </div>
   );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(React.createElement(App));
+root.render(<App />);
