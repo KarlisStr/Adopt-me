967ec3aeddc1020d3b7f0c687c5fe72c7e560180
Karlis Strautnieks <karlisstrautnieks2004@gmail.com>
2025-07-01 16:43:50 +0300

    SearchParams hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
+      <SearchParams />
       <Pet name="Luna" animal="dog" breed="Havanese" />
       <Pet name="Pepper" animal="Cat" breed="mixd" />
       <Pet name="Doink" animal="bird" breed="cockateil" />
